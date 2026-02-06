c99fdd74ff1381c2a027658c959edaf3bda46bdd
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2026-02-07 00:55:51 +0600

    integrate Better Auth with Prisma and email verification

---
+++ prisma/migrations/20260206094708_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('STUDENT', 'TUTOR', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'BANNED');
---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
+import express, { Application } from "express";
+import { auth } from "./lib/auth";
+
+const app: Application = express();
+
+app.use(
+    cors({
+        origin: process.env.APP_URL || "http://localhost:5000",
+        credentials: true,
+    }),
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+export default app;
